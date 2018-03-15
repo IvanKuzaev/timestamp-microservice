42696e382b2ebc1fc3503a4f589003634495a120
Ivan Kuzaev <ivkuzaev@mail.ru>
2018-03-15 14:14:44 +0400

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 */
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
 app.get("/kuzaev", function (request, response) {
   response.send('Hello, dear admin!');
@@ -36,18 +33,19 @@ app.get("/:time", function (request, response) {
 
   var reNatural = /^(Januar|Februar|March|April|May|June|July|August|September|October|November|December) (\d?\d), (\d\d\d\d)$/;
   var fNatural = reNatural.test(param);
-
+  
   if (fNatural) {//date template is valid, but check if the numbers are  correct
       var paramMonth = RegExp.$1;
       var paramDay = RegExp.$2;
       var paramYear = RegExp.$3;
       date = new Date(param);
-//      if (date.toString() == 'Invalid Date')
-//        fNatural = false;
-      if (paramMonth != months[date.getMonth()] || paramDay != date.getDate() || paramYear != date.getYear)
+      if (date.toString() == 'Invalid Date') 
         fNatural = false;
+      else 
+        if (paramMonth != months[date.getMonth()] || paramDay != date.getDate() || paramYear != date.getFullYear())
+          fNatural = false;
   }
-console.log('fNatural = '+fNatural);
+
   var reUnix = /^\d+$/;
   var fUnix = reUnix.test(param);
 
@@ -69,22 +67,9 @@ console.log('fNatural = '+fNatural);
 
   var obj = {unix: unixStr, natural: naturalStr };
   
-//  response.send(JSON.stringify(obj));
-  response.send(obj);
+  response.send(JSON.stringify(obj));
+//  response.send(obj);
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
